a40c1a631d143063daa07a95074641fd9e91bff0
Dave Blencowe <dblencowe@gmail.com>
2022-08-26 15:47:06 +0100

    feat: adds missing background and title color properties for feeds

--- feed.go
+++ feed.go
@@ -1,22 +1,26 @@
 package monzo
 
 type FeedItem struct {
-	AccountID string
-	Type      string
-	URL       string
-	Title     string
-	Body      string
-	ImageURL  string
+	AccountID       string
+	Type            string
+	URL             string
+	Title           string
+	Body            string
+	ImageURL        string
+	BackgroundColor string
+	TitleColor      string
 }
 
 func (cl *Client) CreateFeedItem(item *FeedItem) error {
 	args := map[string]string{
-		"account_id":        item.AccountID,
-		"type":              item.Type,
-		"url":               item.URL,
-		"params[title]":     item.Title,
-		"params[body]":      item.Body,
-		"params[image_url]": item.ImageURL,
+		"account_id":               item.AccountID,
+		"type":                     item.Type,
+		"url":                      item.URL,
+		"params[title]":            item.Title,
+		"params[body]":             item.Body,
+		"params[image_url]":        item.ImageURL,
+		"params[background_color]": item.BackgroundColor,
+		"params[title_color]":      item.BackgroundColor,
 	}
 	rsp := &map[string]string{}
 	return cl.request("POST", "/feed", args, rsp)
