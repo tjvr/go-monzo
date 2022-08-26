1d1afc691edd74b954a4eb9521edb2488561f261
Dave Blencowe <dblencowe@gmail.com>
2022-08-26 15:34:37 +0100

    feat: adds missing account owner information to accounts

--- accounts.go
+++ accounts.go
@@ -1,13 +1,20 @@
 package monzo
 
+type Person struct {
+	ID                 string `json:"id"`
+	PreferredName      string `json:"preferred_name"`
+	PreferredFirstName string `json:"preferred_first_name"`
+}
+
 type Account struct {
-	ID            string `json:"id"`
-	Closed        bool   `json:"closed"`
-	Created       string `json:"created"`
-	Description   string `json:"description"`
-	Type          string `json:"type"`
-	SortCode      string `json:"sort_code"`
-	AccountNumber string `json:"account_number"`
+	ID            string    `json:"id"`
+	Closed        bool      `json:"closed"`
+	Created       string    `json:"created"`
+	Description   string    `json:"description"`
+	Type          string    `json:"type"`
+	SortCode      string    `json:"sort_code"`
+	AccountNumber string    `json:"account_number"`
+	Owners        []*Person `json:"owners"`
 }
 
 func (cl *Client) Accounts(accountType string) ([]*Account, error) {
